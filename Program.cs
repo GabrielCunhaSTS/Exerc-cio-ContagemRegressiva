b0fb1bf9945f7c9c4ef3f42f094381adb15e6610
Gabriel <ggabrielcunha06@gmail.com>
2025-03-12 17:24:00 -0300

    Duelo, preparar, atirar e fogo

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("O duelo vai começar, pressione qualquer tecla para iniciar:");
+Console.ReadKey();
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.WriteLine("FOGO!");
